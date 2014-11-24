5668211fd3454a7a4124973467510bdc8ba8f89d
Diego Jornada <diego.p.jornada@gmail.com>
2014-11-24 14:09:31 -0200

    Fibonacci Done

--- fib.asm
+++ fib.asm
@@ -1,20 +1,38 @@
 .CODE
     INIT:
         jsr SEQFIBONACCI
+        lda #0
+        sta n1
+        lda #1
+        sta n2
         SEQFIBONACCI:
+            lda p
+            add #2
+            sta p
             lda n
             LOOP:
                 jz  FIM
+                lda n1
+                add n2
+                sta nxt
+                sta p,I
+                lda n2
+                sta n1
+                lda nxt
+                sta n2
+                lda p
+                add #1
+                lda n
                 add #-1
                 sta n
                 jmp INIT
 .ENDCODE
 
 .DATA
-    n:      db  #2      ;Seq FIB
+    n:      db  #7      ;Seq FIB
     n1:     db  #0
     n2:     db  #1
     nxt:    db  #0
     p:      db  #vet
-    vet:    db  #0, #1, #0, #0, #0, #0, #0, #0
+    vet:    db  #0, #1, #0, #0, #0, #0, #0
 .ENDDATA
